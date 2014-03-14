80eabc02ec6b19f9bd5301330f730100f2bab751
jeevitha <jeevitha.r.agaram@gmail.com>
2014-03-13 22:37:25 -0700

    Made few changes in add_student functionality.

--- app/add_student.c
+++ app/add_student.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include "../inc/student.h"
 
+#define PATH /home/jeevitha/schooldb_03_12_2014/app/studentinfo.dbs
+
 schooldb_error_t
 db_add_student (student_t *s)
 {
     FILE *my_stream;
-    
-    my_stream = fopen("/home/jeevitha/schooldb_03_12_2014/app/studentinfo.dbs", "a+");
+    int written_bytes;
 
-    fprintf(my_stream, "%50s %50s %50s %100s %10u %50s %50s %5u %c %3u %2u %2u %4u\n",s.name, s.father_name, s.mother_name, s.addr.street_name, s.addr.house_num, s.addr.city, s.addr.state, s.addr.zip_code, s.gender, s.age, s.dob.date, s.dob.month, s.dob.year);
+    my_stream = fopen("/home/jeevitha/schooldb_03_12_2014/app/studentinfo.dbs", "a+");
+    if (my_stream == NULL) {
+        printf("Error in opening file\n");
+        return -1;
+    }
+
+    written_bytes = fprintf(my_stream, "%50s %50s %50s %100s %10u %50s %50s %5u %c %3u %2u %2u %4u\n",s.name, s.father_name, s.mother_name, s.addr.street_name, s.addr.house_num, s.addr.city, s.addr.state, s.addr.zip_code, s.gender, s.age, s.dob.date, s.dob.month, s.dob.year);
+    if(written_bytes <= 0) {
+        printf("Error in writting\n");
+    }
 
     fclose(my_stream);
 }    
